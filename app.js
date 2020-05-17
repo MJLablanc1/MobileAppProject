614717d66e4cb7c725d3c63a141a5a0c60882d78
MJLablanc1 <MJLablanc1@gmail.com>
2020-05-17 14:59:47 -0700

    removed uneeded elements

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var stuff = require('./routes/stuff');
-var skaterouter = require('./routes/skate');
 
 var app = express();
 
@@ -23,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/flowershop', stuff);
-app.use('/skategear', skaterouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
